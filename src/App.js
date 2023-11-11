8abe93534e87e035f1b0f3bd81fd0dea0255bf7f
jihyun han <anyxkr@gmail.com>
2023-11-12 02:28:08 +0900

    Customer 컴포넌트 구조화

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
 import './App.css';
 import Customer from './components/Customer';
 
-const customer = {
-  name: '홍길동',
-  birthday: '961222',
-  gender: '남자',
-  job: '대학생',
-};
+const customers = [
+  {
+    id: 1,
+    image: 'https://picsum.photos/200/150?1',
+    name: '홍길동',
+    birthday: '961222',
+    gender: '남자',
+    job: '대학생',
+  },
+  {
+    id: 2,
+    image: 'https://picsum.photos/200/150?2',
+    name: '이순신',
+    birthday: '930211',
+    gender: '남자',
+    job: '디자이너',
+  },
+  {
+    id: 3,
+    image: 'https://picsum.photos/200/150?3',
+    name: '트와이스',
+    birthday: '970303',
+    gender: '걸그룹',
+    job: '가수',
+  }
+];
 
 function App() {
   return (
-    <Customer
-     name={customer.name}
-     birthday={customer.birthday}
-     gender={customer.gender}
-     job={customer.job}
-     />
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+              key={c.id} 
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+              />
+          );
+        })
+      }
+    </div>
   );
 }
 
